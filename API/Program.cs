b017f71b364678f281960da4f78f593fe5ab85c4
Damian Amalraj <damian.amalraj@hotmail.com>
2024-01-24 20:45:51 +0100

    Revert "Add Data, Models and DbContext to the project"
    This reverts commit bc0448278fd34da6eed2e51ef93a997a3a826e51.

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using API.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationContext>(opt => opt.UseSqlServer(connectionString));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
