bc0448278fd34da6eed2e51ef93a997a3a826e51
Damian Amalraj <damian.amalraj@hotmail.com>
2024-01-24 20:17:24 +0100

    Add Data, Models and DbContext to the project

---
+++ API/Models/Artist.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+
+  public class Artist
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+  }
+}
